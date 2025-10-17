707ec78753e2a0ef9db46433335602a46eae9260
elotex0 <justin.kettner14@icloud.com>
2025-10-17 16:10:11 +0200

    Update generate_pngs.py

--- scripts/generate_pngs.py
+++ scripts/generate_pngs.py
@@ -365,7 +365,6 @@ for filename in sorted(os.listdir(data_dir)):
     if var_type == "t2m":
         im = ax.pcolormesh(lon, lat, data, cmap=t2m_colors, norm=t2m_norm, shading="auto")
 
-        contours = ax.contour(lon, lat, data, levels=t2m_bounds, colors='black', linewidths=0.3, alpha=0.6)
         # Anzahl der Werte, die angezeigt werden sollen
         n_labels = 40
         
